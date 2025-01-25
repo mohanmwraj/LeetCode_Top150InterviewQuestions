5837393b438145af377fd929115da93e7edc94e9
Mohan Raj <mohanmwraj@gmail.com>
2025-01-25 05:53:18 +0530

    Binary Search

--- binarySearch/searchinRotatedSortedArray.java
+++ binarySearch/searchinRotatedSortedArray.java
@@ -1,4 +1,149 @@
 package binarySearch;
-
+/*
+    https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ */
 public class searchinRotatedSortedArray {
+    /*
+        * Approach: Find Pivot Index + Binary Search
+        *
+     */
+    public int search_1(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+
+        // Find the index of the pivot element (the smallest element)
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // Binary search over elements on the pivot element's left
+        int answer = binarySearch_1(nums, 0, left - 1, target);
+        if (answer != -1) {
+            return answer;
+        }
+
+        // Binary search over elements on the pivot element's right
+        return binarySearch_1(nums, left, n - 1, target);
+    }
+
+    // Binary search over an inclusive range [left_boundary ~ right_boundary]
+    private int binarySearch_1(
+            int[] nums,
+            int leftBoundary,
+            int rightBoundary,
+            int target
+    ) {
+        int left = leftBoundary, right = rightBoundary;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /*
+        Time Complexity: O(log N)
+        Space Complexity: O(1)
+     */
+    /*
+        * Approach: Find Pivot Index + Binary Search with Shift
+        *
+     */
+    public int search_2(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+
+        // Find the index of the pivot element (the smallest element)
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return shiftedBinarySearch_2(nums, left, target);
+    }
+
+    // Shift elements in a circular manner, with the pivot element at index 0.
+    // Then perform a regular binary search
+    private int shiftedBinarySearch_2(int[] nums, int pivot, int target) {
+        int n = nums.length;
+        int shift = n - pivot;
+        int left = (pivot + shift) % n;
+        int right = (pivot - 1 + shift) % n;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[(mid - shift + n) % n] == target) {
+                return (mid - shift + n) % n;
+            } else if (nums[(mid - shift + n) % n] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /*
+        Time Complexity: O(log N)
+        Space Complexity: O(1)
+     */
+
+    /*
+        * Approach: One Binary Search
+        *
+        * If the subarray from mid left, is sorted then do BS on left subarray,
+        * else on right subarray.
+        *
+     */
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Case 1: find target
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // Case 2: subarray on mid's left is sorted
+            else if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Case 3: subarray on mid's right is sorted
+            else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /*
+        Time Complexity: O(log N)
+        Space Complexity: O(1)
+     */
 }
