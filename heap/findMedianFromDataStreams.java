d5e7be4bd947b4760c6b1afb126c00568e7f944b
Mohan Raj <mohanmwraj@gmail.com>
2025-01-25 10:47:42 +0530

    Heap

--- heap/findMedianFromDataStreams.java
+++ heap/findMedianFromDataStreams.java
@@ -1,27 +1,45 @@
 package heap;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /*
-    https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+    https://leetcode.com/problems/find-median-from-data-stream/description/
  */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class findMedianFromDataStreams {
+    /*
+        * Approach: Two Heaps
+        *
+        * A max-heap to store the smaller half of the input numbers
+        * A min-heap to store the larger half of the input numbers
+        *
+     */
+    private PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> maxHeap;
 
+    public findMedianFromDataStreams() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+
+    public void addNum(int num) {
+        maxHeap.add(num);
+        minHeap.add(maxHeap.remove());
+
+        if(maxHeap.size() < minHeap.size()){
+            maxHeap.add(minHeap.remove());
+        }
+    }
+
+    public double findMedian() {
+        if(maxHeap.size() > minHeap.size()){
+            return maxHeap.peek();
+        }
+
+        return (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+    /*
+        Time Complexity: O(log N)
+        Space Complexity: O(n)
+     */
 }
