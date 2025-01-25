c80aa052833afcdd4a21b89562c5ec05973f5441
Mohan Raj <mohanmwraj@gmail.com>
2025-01-25 09:24:54 +0530

    Heap

---
+++ heap/findMedianFromDataStreams.java
@@ -0,0 +1,27 @@
+package heap;
+
+/*
+    https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class findMedianFromDataStreams {
+
+}
