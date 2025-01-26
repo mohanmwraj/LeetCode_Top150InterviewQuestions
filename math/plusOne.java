eb6364631fefcbad4e72a933a151dc00b6b38de4
Mohan Raj <mohanmwraj@gmail.com>
2025-01-26 11:06:46 +0530

    Math

--- math/plusOne.java
+++ math/plusOne.java
@@ -1,4 +1,36 @@
 package math;
+/*
+    https://leetcode.com/problems/plus-one/description/
 
+    Input: digits = [1,2,3]
+    Output: [1,2,4]
+ */
 public class plusOne {
+    /*
+        * Approach: Schoolbook Addition with Carry
+        *
+        *
+     */
+
+    public int[] plusOne(int[] digits){
+        int n = digits.length;
+        for(int idx = n - 1; idx >= 0; idx--){
+            if(digits[idx] == 9){
+                digits[idx] = 0;
+            } else {
+                digits[idx]++;
+                return digits;
+            }
+        }
+
+        //We're here because all the digits ar nines.
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    /*
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+     */
 }
